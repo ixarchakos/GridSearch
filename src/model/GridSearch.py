45d876eed4b4d611c9a01ee7394c8845c525e69b
ioannisxar <zannisxarchakos@hotmail.com>
2017-02-22 13:19:45 +0200

    [DEV] split the functionality of Grid fit in functions

--- src/model/GridSearch.py
+++ src/model/GridSearch.py
@@ -1,5 +1,4 @@
 import itertools
-from sklearn import *
 
 
 class GridSearch:
@@ -17,26 +16,46 @@ class GridSearch:
         self.n_top = n_top
 
     def fit(self, x, y, thres):
-        key_list = list()
-        values_list = list()
+        values_list, key_list = self.parameters_to_grid(thres)
+
+        # iterate per model
+        for tuples in list(itertools.product(*values_list)):
+            # extract model parameters and cut off boundary
+            parameters_dict, cut_off_boundary = self.extract_models_parameters(tuples, key_list)
+
+            # check param compatibility with the selected model
+            self.check_param_compatibility(self.algorithm.set_params(**parameters_dict))
+
+            # n-times
+            for i in range(0, self.n_times, 1):
+                # k-fold
+                for j in range(0, self.k_folds, 1):
+
+                    print self.algorithm.set_params(**parameters_dict)
+
+    def parameters_to_grid(self, thres):
+        values_list, key_list = list(), list()
         for key, value_list in self.param_grid.iteritems():
             key_list.append(key)
             values_list.append(value_list)
         values_list.append(thres)
         key_list.append('threshold')
-        for tuples in list(itertools.product(*values_list)):
-            parameters_dict = dict()
-            for i, t in enumerate(tuples):
-                parameters_dict[key_list[i]] = str(t)
-            cut_off_boundary = parameters_dict['threshold']
-            del parameters_dict['threshold']
-
-            for i in range(0, self.n_times, 1):
-                for j in range(0, self.k_folds, 1):
-                    print self.algorithm.set_params(**parameters_dict)
+        return values_list, key_list
 
     @staticmethod
-    def check_param_compatibility(self):
-        print self.algorithm
-        if False:
+    def extract_models_parameters(tuples, key_list):
+        parameters_dict = dict()
+        for i, t in enumerate(tuples):
+            parameters_dict[key_list[i]] = str(t)
+        cut_off_boundary = parameters_dict['threshold']
+        del parameters_dict['threshold']
+        return parameters_dict, cut_off_boundary
+
+    @staticmethod
+    def check_param_compatibility(algorithm):
+        try:
+            algorithm
+        except Exception as e:
+            print e
             print 'Incompatible grid parameters'
+            exit()
