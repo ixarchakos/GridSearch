1552ccaf89d9ab29241d865a49b725ee082d562d
ioannisxar <zannisxarchakos@hotmail.com>
2017-03-13 23:05:12 +0200

    [DEV] quartiles calculation

--- src/tools/general_tools.py
+++ src/tools/general_tools.py
@@ -1,17 +1,29 @@
-from numpy import median
+from numpy import median, percentile
 
 
-def max_value(l):
-    return max(l)
+def max_value(lst):
+    return max(lst)
 
 
-def min_value(l):
-    return min(l)
+def min_value(lst):
+    return min(lst)
 
 
-def mean_value(l):
-    return float(reduce(lambda x, y: x + y, l)) / float(len(l))
+def mean_value(lst):
+    return float(reduce(lambda x, y: x + y, lst)) / float(len(l))
 
 
 def median_value(lst):
     return median(lst)
+
+
+def first_quartile(lst):
+    print percentile(lst, 25)
+
+
+def second_quartile(lst):
+    print percentile(lst, 50)
+
+
+def third_quartile(lst):
+    print percentile(lst, 75)
