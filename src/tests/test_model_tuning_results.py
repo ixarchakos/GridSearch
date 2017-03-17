602eb26c3e285444d41de970c2d9317f58de04c8
ioannisxar <zannisxarchakos@hotmail.com>
2017-03-18 01:21:01 +0200

    remove test files

--- src/tests/test_model_tuning_results.py
+++
@@ -1,8 +0,0 @@
-from src.tools.sampling import k_fold_sample_data_set
-import numpy as np
-x = [[i, i, i] for i in range(1, 11, 1)]
-y = [i for i in range(11, 21, 1)]
-x_train_list, y_train_list, x_test_list, y_test_list = k_fold_sample_data_set(x, np.array(y), 10)
-
-for j in range(0, 10, 1):
-    print x_test_list[j]
